d3c4d131818414c282a3a7b2dede794a9f75059d
Mohnish <mohnish.a007@gmail.com>
2024-08-14 21:47:02 +0100

    cannot redeclare block scoped variable

--- variablesJS/variable.js
+++ variablesJS/variable.js
@@ -2,5 +2,9 @@ console.log('Variable in JS');
 let userName = 'abc';
 const age = 25;
 var token = 'random';
+let citizen = true;
+let some;
 
-console.log('userName: ' + userName + ' age: ' + age + ' token: ' + token);
+console.log('userName: ' + userName + ' age: ' + age + ' token: ' + token + ' citizen: ' + citizen);
+console.log('the value of "some" variable is ' + some)
+//let some = null;  // we cannot redecalre block scoped varable again 
