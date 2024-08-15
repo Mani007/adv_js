936bf99909bda88b9403041a718cbf02c5829d24
Mohnish <mohnish.a007@gmail.com>
2024-08-16 00:58:06 +0100

    reassigning value to variable

--- variablesJS/variable.js
+++ variablesJS/variable.js
@@ -5,6 +5,8 @@ var token = 'random';
 let citizen = true;
 let some;
 
+userName = 'xyz';   // resetting the variable values with let and var 
+
 console.log('userName: ' + userName + ' age: ' + age + ' token: ' + token + ' citizen: ' + citizen);
 console.log('the value of "some" variable is ' + some)
 //let some = null;  // we cannot redecalre block scoped varable again 
