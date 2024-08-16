d66c786ccc49691a068dcfddce474cb2df114c3e
Mohnish <mohnish.a007@gmail.com>
2024-08-16 01:13:56 +0100

    username reassigned to number type and VSCode failed to identify its changes here

--- variablesJS/variable.js
+++ variablesJS/variable.js
@@ -9,7 +9,7 @@ let citizen = true;
 let some;
 
 let userName = 'xyz';   // resetting the variable values with let and var 
-//userName = 1000 // userName changed to number type and program work fine VS code show wrong type in tooltip. DO NOT TRUST VS CODE
+userName = 1000 // userName changed to number type and program work fine VS code show wrong type in tooltip. DO NOT TRUST VS CODE
 //age = 28;  // this is error as we cannot reassign value to const
 //let userName = 'pqr'; // now this is error as its a block scoping problem
 // now the code work fine as we have redeclared the let variable in proper block scope
