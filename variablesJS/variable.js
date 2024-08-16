15470a6dfc7b018446c255a03657ab0ce790362b
Mohnish <mohnish.a007@gmail.com>
2024-08-16 01:15:25 +0100

    memory allocated with undefined

--- variablesJS/variable.js
+++ variablesJS/variable.js
@@ -6,10 +6,10 @@ console.log('Variable in JS');
 const age = 25;
 var token = 'random';
 let citizen = true;
-let some;
+let some;  // memory allocated as undefined
 
 let userName = 'xyz';   // resetting the variable values with let and var 
-userName = 1000 // userName changed to number type and program work fine VS code show wrong type in tooltip. DO NOT TRUST VS CODE
+//userName = 1000 // userName changed to number type and program work fine VS code show wrong type in tooltip. DO NOT TRUST VS CODE
 //age = 28;  // this is error as we cannot reassign value to const
 //let userName = 'pqr'; // now this is error as its a block scoping problem
 // now the code work fine as we have redeclared the let variable in proper block scope
