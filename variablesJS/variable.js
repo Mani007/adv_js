490685c55f5e7975c001cc20bf0a2264f860dba1
Mohnish <mohnish.a007@gmail.com>
2024-08-16 01:04:08 +0100

    block scoping problem with let variable

--- variablesJS/variable.js
+++ variablesJS/variable.js
@@ -1,13 +1,18 @@
 console.log('Variable in JS'); 
-let userName = 'abc';
+{ //This is called block scoping
+    let userName = 'abc';
+
+}
 const age = 25;
 var token = 'random';
 let citizen = true;
 let some;
+{
 
-userName = 'xyz';   // resetting the variable values with let and var 
-age = 28;  // this is error as we cannot reassign value to const
-
+    userName = 'xyz';   // resetting the variable values with let and var 
+}
+//age = 28;  // this is error as we cannot reassign value to const
+let userName = 'pqr'; // now this is error as its a block scoping problem
 console.log('userName: ' + userName + ' age: ' + age + ' token: ' + token + ' citizen: ' + citizen);
 console.log('the value of "some" variable is ' + some)
 //let some = null;  // we cannot redecalre block scoped varable again 
