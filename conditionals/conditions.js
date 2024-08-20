76608d1c7657e9f2ef6fa3c3f7b66243332e84dc
Mohnish <mohnish.a007@gmail.com>
2024-08-20 22:50:21 +0100

    comparision with == implicit conversion of string to int behind the scene

--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -1,4 +1,6 @@
 console.log("Understanding Comparision operator in Details")
 
-const userAge1 = '25'
+const userAge1 = '30'
 const userAge2 = 30
+
+console.log('Is userAge1 equal to userAge2?', userAge1 == userAge2) // true
