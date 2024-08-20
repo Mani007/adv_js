bc3ca1965aa1c501221b9168eb2814f2274f80cf
Mohnish <mohnish.a007@gmail.com>
2024-08-20 23:02:48 +0100

    comparision with >< both have false

--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -4,3 +4,5 @@ const userAge1 = '30'
 const userAge2 = 30
 // type conversion to int is happening
 console.log('Is userAge1 not equal to userAge2?', userAge1 !== userAge2) // false
+console.log('Is userAge1 > userAge2?', userAge1 > userAge2) 
+console.log('Is userAge1 < userAge2?', userAge1 > userAge2) 
