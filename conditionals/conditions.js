e3678e9b4ea5b268a9d9c50658e5d296d7228aa8
Mohnish <mohnish.a007@gmail.com>
2024-08-20 22:51:20 +0100

    comparision with === strict matching string to int behind the scene hence false

--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -3,4 +3,4 @@ console.log("Understanding Comparision operator in Details")
 const userAge1 = '30'
 const userAge2 = 30
 
-console.log('Is userAge1 equal to userAge2?', userAge1 == userAge2) // true
+console.log('Is userAge1 equal to userAge2?', userAge1 === userAge2) // false
