6b32384a1f29de857e3076723c0a119184ac6c0e
Mohnish <mohnish.a007@gmail.com>
2024-08-20 23:04:31 +0100

    comparision with >< automatic type conversion is happening and both false because they are equal

--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -1,6 +1,6 @@
 console.log("Understanding Comparision operator in Details")
 
-const userAge1 = '29'
+const userAge1 = '30'
 const userAge2 = 30
 // type conversion to int is happening
 console.log('Is userAge1 not equal to userAge2?', userAge1 !== userAge2) // false
