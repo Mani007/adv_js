1e4aa0fca083de35204ad6f7050cef3f1932c604
Mohnish <mohnish.a007@gmail.com>
2024-08-20 23:38:01 +0100

    basic logical operator setup

--- conditionals/Readme.md
+++ conditionals/Readme.md
@@ -5,8 +5,16 @@
 - === STRICT EQUALITY object equality operator, you have to be explicit in type checking
 - != not equality operator
 - !== STRICT object not equality operator
-- ! not operator
+
 - '>' greater than operator
 - < less than operator
 - <= less than equal to operator
-- '>=' greater than equal to equal to operator
+- '>=' greater than equal to equal to operator   
+
+Some important note >> is called right shift operator and << is left shift operator. It operates in binary.    
+
+### Logical operator in JS   
+Although logical operator look simple but behind the scene it can be very challenging for some operations.    
+- ! not operator
+- && and operator   Both true
+- || or operator  One of them true
--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -1,8 +1,14 @@
-console.log("Understanding Comparision operator in Details")
+console.log("Understanding Logical operator in Details")
 
-const userAge1 = '30'
-const userAge2 = 30
+// const userAge1 = '30'
+// const userAge2 = 30
 // type conversion to int is happening
-console.log('Is userAge1 not equal to userAge2?', userAge1 !== userAge2) // false
-console.log('Is userAge1 > userAge2?', userAge1 > userAge2) 
-console.log('Is userAge1 < userAge2?', userAge1 < userAge2) 
+//console.log('Is userAge1 not equal to userAge2?', userAge1 !== userAge2) 
+// console.log('Is userAge1 > userAge2?', userAge1 > userAge2) 
+// console.log('Is userAge1 < userAge2?', userAge1 < userAge2)  
+console.log('The logical AND',(7>4) && (7<10))
+console.log('The logical AND with false output',(7>4) && (7<4))
+console.log('The logical OR with false output',(7<4) || (7<4))
+console.log('The logical OR with true output because one of the value is true ',(7>4) || (7<4))
+
+
