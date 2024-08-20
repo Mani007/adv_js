c13ed3bc47b7e8f8e17ca0467f3d8f66785dd947
Mohnish <mohnish.a007@gmail.com>
2024-08-20 23:00:23 +0100

    comparision with !== strict not equal matching string to int  hence true

--- conditionals/conditions.js
+++ conditionals/conditions.js
@@ -3,4 +3,4 @@ console.log("Understanding Comparision operator in Details")
 const userAge1 = '30'
 const userAge2 = 30
 // type conversion to int is happening
-console.log('Is userAge1 equal to userAge2?', userAge1 != userAge2) // false
+console.log('Is userAge1 not equal to userAge2?', userAge1 !== userAge2) // false
