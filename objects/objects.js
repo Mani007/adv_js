730fbd1103cf1495e629cf17d290417ce8141145
Mohnish <mohnish.a007@gmail.com>
2024-08-31 19:39:05 +0100

    Objects creation

--- objects/objects.js
+++ objects/objects.js
@@ -1,3 +1,8 @@
 console.log('objects in JS is the only non primitive datatypes ')  
-const myObj = {} 
+const myObj = { 
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    city: 'New York'  // Key-Value Pairs
+} 
 console.log(myObj)
