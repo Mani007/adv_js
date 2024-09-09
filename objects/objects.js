9905077d0faa902d4e9f0ace0303db73ea39f997
Mohnish <mohnish.a007@gmail.com>
2024-09-09 23:35:29 +0100

    Onjects check the context of different objects - check the memory snapshot

--- objects/objects.js
+++ objects/objects.js
@@ -1,15 +1,23 @@
-console.log('objects in JS is the only non primitive datatypes ')  
-const myObj = { 
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    city: 'New York'  // Key-Value Pairs
-} 
-console.log(myObj)
-const myObj2 = { 
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    city: 'New York'  // Key-Value Pairs
-} 
-console.log(myObj2)
+// console.log('objects in JS is the only non primitive datatypes ')  
+// const myObj = { 
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     city: 'New York'  // Key-Value Pairs
+// } 
+// console.log(myObj)
+// const myObj2 = { 
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     city: 'New York'  // Key-Value Pairs
+// } 
+// console.log(myObj2)
+const myName = 'abcde';
+const myObj1 = { }
+const myObj2 = { }
+const name1='';
+const name2='';
+const age1=25;
+const age2=30;  
+// Please check the context above, very interesting memory footprint 
