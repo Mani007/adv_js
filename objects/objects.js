2b4dbfdec97965c18cc9c3ba2f82afedbcfad6ae
Mohnish <mohnish.a007@gmail.com>
2024-09-09 23:43:10 +0100

    Onjects attribute access using myObj['my-name'] notation

--- objects/objects.js
+++ objects/objects.js
@@ -14,8 +14,18 @@
 // } 
 // console.log(myObj2)
 const myName = 'abcde';
-const myObj1 = { }
-const myObj2 = { }
+const myObj1 = {
+    name1: 'abcde',
+    age1: 30,
+ }
+const myObj2 = {
+    'name-2': 'abcde',
+    age2: 20,
+ }
+console.log(myObj1.name1);
+console.log(myObj1['name1']);
+console.log(myObj2['name-2']);
+console.log(myObj2.name-2);  // will not work properly output is NaN so instead use myobj2['name-2']
 const name1='';
 const name2='';
 const age1=25;
