ced663df0d3e920717e091383061dd0974bac3cb
Mohnish <mohnish.a007@gmail.com>
2024-09-10 22:36:57 +0100

    Onjects attribute access using myObj['myVariable'] notation

--- objects/objects.js
+++ objects/objects.js
@@ -14,6 +14,7 @@
 // } 
 // console.log(myObj2)
 const myName = 'abcde';
+const myVariable = 'myVar'  // myVar is key on myObj2
 const myObj1 = {
     name1: 'abcde',
     age1: 30,
@@ -21,10 +22,14 @@ const myObj1 = {
 const myObj2 = {
     'name-2': 'abcde',
     age2: 20,
+    myVar: 'some value',
  }
 console.log(myObj1.name1);
 console.log(myObj1['name1']);
 console.log(myObj2['name-2']);
+console.log(myObj2.myVariable);  // This will give undefined
+console.log(myObj2['myVariable']); // This will give output as "some value". note [] brackets use
+console.log(myObj2.some);  // this will give undefined as result 
 console.log(myObj2.name-2);  // will not work properly output is NaN so instead use myobj2['name-2']
 const name1='';
 const name2='';
