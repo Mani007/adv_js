2a0af30b78234bc45f5cc010e1e38b23786691ce
Mohnish <mohnish.a007@gmail.com>
2024-09-11 06:56:10 +0100

    adding new attributes to the objects with dot and [] style

--- objects/objects.js
+++ objects/objects.js
@@ -19,21 +19,30 @@ const myObj1 = {
     name1: 'abcde',
     age1: 30,
  }
-const myObj2 = {
+// const myObj2 = {
+//     'name-2': 'abcde',
+//     age2: 20,
+//     myVar: 'some value',
+//  }
+ const myObj2 = {
     'name-2': 'abcde',
     age2: 20,
-    myVar: 'some value',
+   
  }
-console.log(myObj1.name1);
-console.log(myObj1['name1']);
-console.log(myObj2['name-2']);
-console.log(myObj2['name'+'-2']); // expression is evaluated to make key of myObj2
-console.log(myObj2.myVariable);  // This will give undefined
-console.log(myObj2['myVariable']); // This will give output as "some value". note [] brackets use
-console.log(myObj2.some);  // this will give undefined as result 
-console.log(myObj2.name-2);  // will not work properly output is NaN so instead use myobj2['name-2']
-const name1='';
-const name2='';
-const age1=25;
-const age2=30;  
+ myObj2.lastName = 'surname';
+ myObj1['lastName'] = 'my surname';
+ console.log(myObj1.lastName);
+ console.log(myObj2['lastName']);
+// console.log(myObj1.name1);
+// console.log(myObj1['name1']);
+// console.log(myObj2['name-2']);
+// console.log(myObj2['name'+'-2']); // expression is evaluated to make key of myObj2
+// console.log(myObj2.myVariable);  // This will give undefined
+// console.log(myObj2['myVariable']); // This will give output as "some value". note [] brackets use
+// console.log(myObj2.some);  // this will give undefined as result 
+// console.log(myObj2.name-2);  // will not work properly output is NaN so instead use myobj2['name-2']
+// const name1='';
+// const name2='';
+// const age1=25;
+// const age2=30;  
 // Please check the context above, very interesting memory footprint 
