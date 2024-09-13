a099a783b35d96a450c1a63f018c824cba0d4cbd
Mohnish <mohnish.a007@gmail.com>
2024-09-13 22:37:42 +0100

    Arrays in JS :built-in function concat

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,3 +1,4 @@
 const arr = ['a', 'b', 'c', 'd', 'e',];
 console.log(arr);
 console.log(`Type of array is ${typeof(arr)}`);
+console.log(`Concat with built-in function of array is ${arr.concat('f')}`);
