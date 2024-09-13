ef3ef68929292629d69bb9d46d01748784d7368d
Mohnish <mohnish.a007@gmail.com>
2024-09-13 22:40:37 +0100

    Arrays in JS: accessing elements

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,4 +1,5 @@
 const arr = ['a', 'b', 'c', 'd', 'e',];
 console.log(arr);
 console.log(`Type of array is ${typeof(arr)}`);
+console.log(`Access of array is ${arr[2]}`);  // output c from index 3
 console.log(`Concat with built-in function of array is ${arr.concat('f')}`);
