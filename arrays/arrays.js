79b8eee6f1f3bec25f8473dccc60c67c8c0946c5
Mohnish <mohnish.a007@gmail.com>
2024-09-13 22:45:30 +0100

    Arrays in JS: adding new value at certain index

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -2,5 +2,6 @@ const arr = ['a', 'b', 'c', 'd', 'e',];
 console.log(arr);
 console.log(`Type of array is ${typeof(arr)}`);
 console.log(`Access of array is ${arr[2]}`);  // output c from index 3
-console.log(`Update value inside of array is ${arr[2]='X'}`);  // output c from index 3
+console.log(`Update value inside of array is ${arr[2]='X'}`);  
+console.log(`Update value inside of array is ${arr[9]='XXX'}`); // adding new value at certain index 
 console.log(`Concat with built-in function of array is ${arr.concat('f')}`);
