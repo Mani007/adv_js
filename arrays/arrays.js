52936d8fb8222db2fba956f215f71d82e4333cba
Mohnish <mohnish.a007@gmail.com>
2024-09-14 22:45:03 +0100

    Arrays in JS: Changing value in  multi dimensional array

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -21,3 +21,4 @@
 const multiArray = [['abc',2],['pqr',4],['xy',6],['vvv',8],];
 console.log(multiArray)
 console.log(`Multidimensional array ${multiArray}`)
+console.log(`Multidimensional array adding element  ${multiArray[2][0] = 'kkk'} `)
