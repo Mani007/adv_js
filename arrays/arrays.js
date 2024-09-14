68eea7c47bbc7dcbb396b36c61c2452e6c31c6f6
Mohnish <mohnish.a007@gmail.com>
2024-09-14 22:51:52 +0100

    Arrays in JS: adding new element to multi array is working

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -21,6 +21,6 @@
 const multiArray = [['abc',2],['pqr',4],['xy',6],['vvv',8],];
 console.log(multiArray)
 console.log(`Multidimensional array ${multiArray}`)
-multiArray[5][0] = 'kkk'
+//multiArray[5][0] = 'kkk'  // Adding element in multi dimension array is very critical
 console.log(multiArray)
-console.log(`Multidimensional array adding element  ${multiArray[5][0] = 'kkk'} `)
+console.log(`Multidimensional array adding element  ${multiArray[5] = ['kkk',12]} `)
