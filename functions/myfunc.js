035a47732081a5bb27cdba77b9ae22580157cb59
Mohnish <mohnish.a007@gmail.com>
2024-09-16 03:08:38 +0100

    Functions in JS: function multiple parameter and called with argument

--- functions/myfunc.js
+++ functions/myfunc.js
@@ -1,10 +1,10 @@
 console.log('Functions in JS')
-function intro(names = 'blank'){
-    console.log('Namaste!!! ' + names)
+function intro(names = 'blank', age = 18){  // parameter we define in the function
+    console.log('Namaste!!! ' + names + ' ' + age)
 }
 console.log('****************************************************************')
-intro('bob');
+intro('bob', 25); // argument we pass to the function
 console.log('****************************************************************')
-intro('the builder');
+intro('the builder', 35);
 console.log('****************************************************************')
 intro();
