90b447e62b522aeda17f3072ddeb37ebf6207de2
Mohnish <mohnish.a007@gmail.com>
2024-09-16 02:34:00 +0100

    Loops in JS: basic for do while loop

--- loops/loops.js
+++ loops/loops.js
@@ -1,22 +1,26 @@
 console.log('Program Started......')
 //let num=0;
-let names = ['a', 'b', 'c', 'd', 'e', 'f'];
+//let names = ['a', 'b', 'c', 'd', 'e', 'f'];
 // while (num < name.length) {
 //     console.log('Number is: ', num)
 //     console.log('Name is: ', name[num])
 //     num++;
 // }
 
-for (let i = 0; i < names.length; i++) {
-    // console.log(`Value of i is ${i}`)
-    // if (i % 2 === 0){
+// for (let i = 0; i < names.length; i++) {
+     // console.log(`Value of i is ${i}`)
+     // if (i % 2 === 0){
     //     console.log(`Value of i is ${i} is even`)
     // }
     // else {
     //     console.log(`Value of i is ${i} is odd`)
     // }
-    console.log(`the name index is ${i} with value ${names[i]}`)
-}
+//     console.log(`the name index is ${i} with value ${names[i]}`)
+// }
+let i = 10 
+do {
+ console.log('Do while loop run at least one time irrespective of condition of loops')
+} while (i < 5)
 console.log('Program Finished')
 // while (true) { // infinite loop
 //     console.log('hi')
