2d70de79149c7d7025cb353736980c011b4af3be
Mohnish <mohnish.a007@gmail.com>
2024-09-16 02:25:56 +0100

    Loops in JS: basic for loop

--- loops/loops.js
+++ loops/loops.js
@@ -1,10 +1,13 @@
 console.log('Program Started......')
-let num=0;
-let name = ['a', 'b', 'c', 'd', 'e', 'f'];
-while (num < name.length) {
-    console.log('Number is: ', num)
-    console.log('Name is: ', name[num])
-    num++;
+//let num=0;
+// let name = ['a', 'b', 'c', 'd', 'e', 'f'];
+// while (num < name.length) {
+//     console.log('Number is: ', num)
+//     console.log('Name is: ', name[num])
+//     num++;
+// }
+for (let i = 0; i < 10; i++) {
+    console.log(`Value of i is ${i}`)
 }
 console.log('Program Finished')
 // while (true) { // infinite loop
