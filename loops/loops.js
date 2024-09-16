a5792d9614476b0a2bab855156e329cd3775dac9
Mohnish <mohnish.a007@gmail.com>
2024-09-16 02:30:58 +0100

    Loops in JS: basic for loop on array

--- loops/loops.js
+++ loops/loops.js
@@ -1,20 +1,21 @@
 console.log('Program Started......')
 //let num=0;
-// let name = ['a', 'b', 'c', 'd', 'e', 'f'];
+let names = ['a', 'b', 'c', 'd', 'e', 'f'];
 // while (num < name.length) {
 //     console.log('Number is: ', num)
 //     console.log('Name is: ', name[num])
 //     num++;
 // }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < names.length; i++) {
     // console.log(`Value of i is ${i}`)
-    if (i % 2 === 0){
-        console.log(`Value of i is ${i} is even`)
-    }
-    else {
-        console.log(`Value of i is ${i} is odd`)
-    }
+    // if (i % 2 === 0){
+    //     console.log(`Value of i is ${i} is even`)
+    // }
+    // else {
+    //     console.log(`Value of i is ${i} is odd`)
+    // }
+    console.log(`the name index is ${i} with value ${names[i]}`)
 }
 console.log('Program Finished')
 // while (true) { // infinite loop
