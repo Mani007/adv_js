a3b1e577c0a5044f8faff4c07b9d5e291801ba5e
Mohnish <mohnish.a007@gmail.com>
2024-09-16 01:48:42 +0100

    Loops in JS: basic while loop

--- loops/loops.js
+++ loops/loops.js
@@ -1,4 +1,9 @@
 console.log('Program Started......')
+let num = 0;
+while (num < 10) {
+    console.log('Number is: ', num)
+    num++;
+}
 // while (true) { // infinite loop
 //     console.log('hi')
 // }
