a4b2d43c726af42ba88ff7963ad36ee91ff8c4d6
Mohnish <mohnish.a007@gmail.com>
2024-09-16 11:24:10 +0100

    Functions in JS: Understanding return values

--- functions/myfunc.js
+++ functions/myfunc.js
@@ -1,6 +1,7 @@
 console.log('Functions in JS')
 function intro(names = 'blank', age = 18){  // parameter we define in the function
-    console.log('Namaste!!! ' + names + ' ' + age)
+    console.log('Namaste!!! ' + names + ' ' + age) // Console output is not equivelent to result of a function. 
+    
 }
 console.log('****************************************************************')
 intro('bob', 25); // argument we pass to the function
