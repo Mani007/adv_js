99918233ed0f1d88c744ab1a764fcd0689f16b97
Mohnish <mohnish.a007@gmail.com>
2024-09-17 03:21:36 +0100

    Functions in JS: returning value in obtained

--- functions/myfunc.js
+++ functions/myfunc.js
@@ -1,11 +1,11 @@
 console.log('Functions in JS')
 function intro(names = 'blank', age = 18){  // parameter we define in the function
-    console.log('Namaste!!! ' + names + ' ' + age) // Console output is not equivelent to result of a function. 
-    
+    //console.log('Namaste!!! ' + names + ' ' + age) // Console output is not equivelent to result of a function. 
+    return 'Namaste!!! ' + names + ' ' + age
 }
 console.log('****************************************************************')
-let intro1 = intro('bob', 25); // return will be undefined
+let intro1 = intro('bob', 25); // return will be now available in variable intro1
 console.log('****************************************************************')
-let intro2 = intro('the builder', 35);  // return will be undefined
+let intro2 = intro('the builder', 35);  // return will be now available in variable intro1
 console.log('****************************************************************')
-console.log(intro1 , intro2) // return will be undefined
+console.log(intro1 , intro2) // return will be output
