e131aecbcfe32456c6387c8a09dccc4f5e3121a2
Mohnish <mohnish.a007@gmail.com>
2024-09-18 03:02:48 +0100

    Functions in JS: hoisting problem is not founction calling

--- functions/hoisting.js
+++ functions/hoisting.js
@@ -1,5 +1,9 @@
 console.log('Hoisting Program starts here......')
 // hoisting problem with var when we try to access variable before its declared
 // the variable goes to temporal dead zone
+dj()
 console.log(someuser) // this will not through any error rather return undefined
 var someuser = 'abc' // var create variable at the global scope
+function dj(){
+    console.log('haalo')
+}
