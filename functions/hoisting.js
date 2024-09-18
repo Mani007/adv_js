d1b4173a0ccb61f177321a7e667dc4c7f5a7675c
Mohnish <mohnish.a007@gmail.com>
2024-09-18 03:08:45 +0100

    Functions in JS: function defination and declaration

--- functions/hoisting.js
+++ functions/hoisting.js
@@ -4,6 +4,10 @@ console.log('Hoisting Program starts here......')
 console.log(yourDJ)
 console.log(someuser) // this will not through any error rather return undefined
 var someuser = 'abc' // var create variable at the global scope
-var yourDJ = function dj(){
+var yourDJ = function dj(){  // This is called function expression in function defination
     console.log('haalo')
+}
+
+function myfun(){ // This is called function declaration in function defination
+    console.log('program ends here.....')
 }
