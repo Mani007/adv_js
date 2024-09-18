c26f865d19e855029d923cc7590b2896f1c40abe
Mohnish <mohnish.a007@gmail.com>
2024-09-18 03:10:18 +0100

    Functions in JS: hoisting problem resolved with function

--- functions/hoisting.js
+++ functions/hoisting.js
@@ -1,13 +1,13 @@
 console.log('Hoisting Program starts here......')
 // hoisting problem with var when we try to access variable before its declared
 // the variable goes to temporal dead zone
-console.log(yourDJ)
+
 console.log(someuser) // this will not through any error rather return undefined
 var someuser = 'abc' // var create variable at the global scope
 var yourDJ = function dj(){  // This is called function expression in function defination
     console.log('haalo')
 }
-
+console.log(yourDJ)  // hoiting problems with function is resolved
 function myfun(){ // This is called function declaration in function defination
     console.log('program ends here.....')
 }
